8442818add94e167ae4583a1932b2acab9a872ab
LakshmiNarayana <klnr2205@gmail.com>
2024-04-29 20:10:19 +0530

    Url changed

--- NxtGenCucumber/src/test/java/com/runner/RunnerClass.java
+++ NxtGenCucumber/src/test/java/com/runner/RunnerClass.java
@@ -1,7 +1,6 @@
 package com.runner;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
@@ -10,6 +9,8 @@ import io.cucumber.junit.CucumberOptions;
 		features=".\\src\\test\\java\\com\\featurefiles",
 		glue="com.stepdefinition",
 		dryRun=false,
+		stepNotifications = true,
+		monochrome = true,
 		plugin= {"pretty","html:target/ExecutionReport.html","json:target/ExecutionReport.json","junit:target/ExecutionReport.xml"},
 		tags="@TagTwo")
 public class RunnerClass {
